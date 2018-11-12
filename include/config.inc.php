b045394d0e9a69209345a759664607f0a3a06167
JasonKurtzWebDeveloper <JasonKurtzWebDeveloper@gmail.com>
2018-11-11 21:22:47 -0500

    updated config.inc.php

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,7 +1,10 @@
 <?php
 	// provide the current version of the application
-	define("VERSION", "2.0.3.0");
+	define("VERSION", "2.0.4.0");
 
 	// provide the application name
 	define("SITENAME", "inside out | 2xx");
+
+	// defines the default page of the website
+	define('DEFUALT_PAGE','home');
 ?>
